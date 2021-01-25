6e1e657bff723eb5e4cffadb20fdeb3cb7891239
MArhEV <marthef100@gmail.com>
2021-01-25 19:40:46 +0100

    name formatting

---
+++ UpperLetters.py
@@ -0,0 +1,5 @@
+name1 = 'jan  '
+name2 = ' kowalski  '
+name1 = name1.strip()
+name2 = name2.strip()
+
---
+++ name_letters.py
@@ -0,0 +1,12 @@
+name1 = ' jan  '
+name2 = ' kowalski'
+full_name = f'{name1} {name2}'
+print(full_name)
+
+name1 = name1.strip()
+name2 = name2.strip()
+full_name = f'{name1} {name2}'
+print(full_name)
+
+full_name = full_name.title()
+print(full_name)
